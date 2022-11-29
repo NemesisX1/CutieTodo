dc6a554ba9ba388201c6f19c3f04b0d545818ff8
NemesisX1 <junior.medehou00@gmail.com>
2022-11-29 11:27:08 +0100

    (add) docs

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,112 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/vsLight');
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Docs',
+  tagline: 'A Very Good Project created by Very Good CLI.',
+  url: 'https://docs.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'com.example.verygoodcore', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/com.example.verygoodcore/docs/tree/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Docs',
+        logo: {
+          alt: 'Docs Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'overview',
+            position: 'left',
+            label: 'Overview',
+          },
+          {
+            href: 'https://github.com/com.example.verygoodcore/docs',
+            position: 'right',
+            className: 'navbar-github-icon',
+            'aria-label': 'GitHub repository',
+          },
+        ],
+      },
+      footer: {
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Overview',
+                to: '/docs/overview',
+              },
+            ],
+          },
+          {
+            title: 'Resources',
+            items: [
+              {
+                label: 'Blog',
+                href: '#',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/com.example.verygoodcore/docs',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        additionalLanguages: ['bash', 'dart', 'yaml'],
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
